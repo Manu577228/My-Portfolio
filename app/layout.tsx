5a7460921fd1b8a11a1370a9f953b7290a009439
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-10-17 03:45:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,20 @@ import { ThemeProvider } from "next-themes"
 
 import './globals.css'
 
-//👇 Import Open Sans font
+//👇 Import Montserrat Sans font
 import { Montserrat } from 'next/font/google'
 
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={Montserrat.className}>
+    <html lang="en" className={montserrat.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
